5a4bd8ac27adb4a1eda7a2d513f9a6c866c0cb10
Alfredo Tous <alfredotous2304@gmail.com>
2023-10-23 13:20:38 -0500

    Mensaje de Bienvenida y menu principal

---
+++ Menu.py
@@ -0,0 +1,9 @@
+print("Bienvenido al Restaurante, que deseas hacer?")
+
+i=0
+
+while i==0:
+    print("1. Ver Menu")
+    print("2. Ver pedido")
+    print("3. Eliminar productos")
+    print("4. salir")
